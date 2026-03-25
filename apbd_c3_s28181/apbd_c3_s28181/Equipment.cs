79d45af39150b050211fa5e3ca9e894a72d10cdf
AnnaNejman <s28181@pjwstk.edu.pl>
2026-03-25 21:22:21 +0100

    Add Equipment class

---
+++ apbd_c3_s28181/apbd_c3_s28181/Equipment.cs
@@ -0,0 +1,16 @@
+namespace apbd_c2_s28181;
+
+public abstract class Equipment
+{
+    private static int _idCounter = 1;
+    public int Id { get; }
+    public string Name { get; set; }
+    public EquipmentStatusType Status { get; set; }
+    public string Data="abc";
+    protected Equipment(string name)
+    {
+        Id = _idCounter++;
+        Name = name;
+        Status = EquipmentStatusType.Available;
+    }
+}
---
+++ apbd_c3_s28181/apbd_c3_s28181/EquipmentStatusType.cs
@@ -0,0 +1,8 @@
+namespace apbd_c2_s28181;
+
+public enum EquipmentStatusType
+{
+    Available, 
+    Rented, 
+    Unavailable
+}
