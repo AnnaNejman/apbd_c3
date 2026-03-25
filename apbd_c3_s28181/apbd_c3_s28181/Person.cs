87dd80d4bd6fabe5c0b8c7c52aaedb2eb7e3bfcc
AnnaNejman <s28181@pjwstk.edu.pl>
2026-03-25 21:34:49 +0100

    Create Person.cs

---
+++ apbd_c3_s28181/apbd_c3_s28181/Person.cs
@@ -0,0 +1,16 @@
+namespace apbd_c3_s28181;
+
+public abstract class Person
+{
+    private static int _idCounter = 1;
+    public int Id { get; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    protected Person(string firstName, string lastName)
+    {
+        Id = _idCounter++;
+        FirstName = firstName;
+        LastName = lastName;
+    }
+}
