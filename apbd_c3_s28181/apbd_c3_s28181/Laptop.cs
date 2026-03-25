c4f54cf5708d425a12a5ecee53e35b2300edd549
AnnaNejman <s28181@pjwstk.edu.pl>
2026-03-25 21:26:15 +0100

    Create Laptop.cs

---
+++ apbd_c3_s28181/apbd_c3_s28181/Laptop.cs
@@ -0,0 +1,13 @@
+namespace apbd_c3_s28181;
+
+public class Laptop: Equipment
+{
+    public int ram {get; set;}
+    public string cpu {get; set;}
+
+    public Laptop(string name, int ram, string cpu) : base(name)
+    {
+        this.ram = ram;
+        this.cpu = cpu;
+    }
+}
