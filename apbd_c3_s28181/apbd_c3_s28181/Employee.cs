4dbfafcf83f33a216f755554fb8e7fffa3220bee
AnnaNejman <s28181@pjwstk.edu.pl>
2026-03-25 21:36:26 +0100

    Add student and employee

---
+++ apbd_c3_s28181/apbd_c3_s28181/Employee.cs
@@ -0,0 +1,7 @@
+namespace apbd_c3_s28181;
+
+public class Employee : Person
+{
+    public Employee(string f, string l) : base(f, l) { }
+    
+}
---
+++ apbd_c3_s28181/apbd_c3_s28181/Student.cs
@@ -0,0 +1,7 @@
+namespace apbd_c3_s28181;
+
+public class Student : Person
+{
+    public Student(string f, string l) : base(f, l) { }
+    
+}
